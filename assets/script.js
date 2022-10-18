269a95791d09184ec4a29823e7c62482f33d8b16
Hailey <haileyacarlson@gmail.com>
2022-10-17 23:57:43 -0700

    here

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 ///////////////////////// DO NOT CHANGE ////////////////////////////////////
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//var passwordLength = 8;
+var password = "";
+var type = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*-_";
 
 // Write password to the #password input
 function writePassword() {
@@ -10,9 +13,26 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 ///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
 
 function generatePassword() {
   /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
+  var passwordLength = window.prompt("How many characters would you like in your password?")
+  passwordLength = '';
+
+console.log(passwordLength);
+
+  if ((passwordLength <8) && (passwordLength>128)) {
+    return;
+  }
+
+  
+
+  var characterType = window.prompt("Enter one at a time charecter types needed : upercase, lowercase, numbers, special charecters ")
+
+
+  password = Math.floor(Math.random() * passwordLength) ;
+document.querySelector("#password").innerHTML = password;
+
 }
